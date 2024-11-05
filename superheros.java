8698bbd46954aec56f6195620f13c8e584334357
RC-COOLA <a2jministries@gmail.com>
2024-11-04 22:36:00 -0600

    Update superheros.java

--- superheros.java
+++ superheros.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comarator;
+
 public class SuperheroRoster{
 
     public static void main(String[] args) {
@@ -8,8 +11,12 @@ public class SuperheroRoster{
         int powerlevels [] = {10,8,6};
         String superhero;
         int index;
-        for (int i = 0; i < HeroNames.length; i++) {
-            System.out.println(HeroNames[i]);
+       for (int i = 0; i < HeroNames.length; i++) {
+    System.out.println("Hero: " + HeroNames[i]);
+    System.out.println("Ability: " + abilities[i]);
+    System.out.println("Power Level: " + powerlevels[i]);
+    System.out.println();
+}
             
         }
        System.out.print("Enter your favorite superhero: ");
@@ -26,21 +33,62 @@ public class SuperheroRoster{
        int average = averagePower(powerlevels);
        System.out.println("Average power level is: " + average + ".");
     }
-    public static int findName (String arr[], String arr2[], int arr3[], String key){
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i].compareToIgnoreCase(key) == 0){
-                System.out.println(arr[i] + " Abilities: " + arr2[i] + " Power Level: " + arr3[i]);
-                return i;
-            }
+    public static int findName(String[] names, String[] abilities, int[] powerLevels, String target) {
+    for (int i = 0; i < names.length; i++) {
+        if (names[i].equalsIgnoreCase(target)) {
+            System.out.println("Hero Found:");
+            System.out.println("Name: " + names[i]);
+            System.out.println("Ability: " + abilities[i]);
+            System.out.println("Power Level: " + powerLevels[i]);
+            return i;
         }
-        return -1;
     }
-    public static int averagePower(int arr3[]){
-        int sum = 0, quantity = arr3.length;
-        for(int i = 0; i < quantity; i++){
-            sum = sum + arr3[i];
-        }
-        int avg = sum / quantity;
-        return avg;
-    }    
+    System.out.println("Hero not found.");
+    return -1;
+    }
+class Superhero {
+    private String name;
+    private String ability;
+    private int powerLevel;
+ 
+    public Superhero(String name, String ability, int powerLevel) {
+        this.name = name;
+        this.ability = ability;
+        this.powerLevel = powerLevel;
+    }
+ 
+    public void displayHero() {
+        System.out.println("Name: " + name);
+        System.out.println("Ability: " + ability);
+        System.out.println("Power Level: " + powerLevel);
+    }
+    Superhero[] heroes = {
+    new Superhero("Thor", "Thunder", 10),
+    new Superhero("Iron Man", "Strength", 8),
+    new Superhero("Captain America", "Speed", 6)
+}
+for (Superhero hero : heroes) {
+    hero.displayHero();
+    System.out.println();
+}
+public static void sortHeroesByPower(Superhero[] heroes) {
+    Arrays.sort(heroes, Comparator.comparingInt(Superhero::getPowerLevel).reversed());
+    System.out.println("Heroes sorted by power level (highest to lowest):");
+    for (Superhero hero : heroes) {
+        hero.displayHero();
+        System.out.println();
+    }
+}
+}
+   // Calculated the average power level from the array of power levels//
+public static double calculateAveragePower(int[] powerLevels) {
+    if (powerLevels.length == 0) {
+        System.out.println("No power levels available.");
+        return 0.0;
+    }
+    int sum = 0;
+    for (int level : powerLevels) {
+        sum += level;
+    }
+    return (double) sum / powerLevels.length;
 }
